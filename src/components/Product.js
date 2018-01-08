0eace510fe1aaa1f2c1fc34a4840fd6734dff197
Jake <jakemacnaughton@gmail.com>
2018-01-08 11:49:31 -0500

    Done

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,13 +5,15 @@ import PropTypes from 'prop-types';
 class Product extends React.Component {
   render() {
     return (
-      <div className="wrapper"></div>
-        // <ul>
-          // <li>{this.props.name}</li>
-          // <li>{this.props.producer}</li>
-          // <li>{this.props.hasWatermark}</li>
-          // <li>{this.props.color}</li>
-          // <li>{this.props.weight}</li>
+      <div className="wrapper">
+        <ul>
+          <li>{this.props.name}</li>
+          <li>{this.props.producer}</li>
+          <li>{this.props.hasWatermark}</li>
+          <li>{this.props.color}</li>
+          <li>{this.props.weight}</li>
+        </ul>
+      </div>
     )
   }
 }
